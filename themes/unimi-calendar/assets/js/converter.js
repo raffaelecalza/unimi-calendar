3d88d84b98e254a073c8afd04b11cdf451ff4cbf
Raffaele Calzà <raffaelecalza4@gmail.com>
2022-11-24 15:47:58 +0100

    improved ical conversion

--- themes/unimi-calendar/assets/js/converter.js
+++ themes/unimi-calendar/assets/js/converter.js
@@ -1,10 +1,34 @@
+// get ics calendar url from the webapp url
 function urlChangeEvent() {
-    var url = document.getElementById('url').value
-    var ical = url.replace('index.php', 'export/ec_download_ical_list.php')
+    var urlTxt = document.getElementById('url').value
+    try {
+        var url = new URL(urlTxt)
+        var params = url.searchParams
+
+        // remove useless params
+        params.delete('ar_codes_')
+        params.delete('ar_select_')
+        params.delete('highlighted_date')
+        params.delete('periodo_didattico')
+        params.delete('list')
+        params.delete('col_cells')
+        params.delete('empty_box')
+        params.delete('only_grid')
+        params.delete('week_grid_type')
+        params.delete('all_events')
+        params.delete('faculty_group')
+
+        var ical = url.toString().replace('index.php', 'export/ec_download_ical_list.php')
+    }
+    catch(ex) {
+        ical = invalidURL
+    }
+
     document.getElementById('ical-button').disabled = true
-    if(url == "")
+    
+    if(urlTxt == "")
         ical = ""
-    else if(!url.includes(unimiURL))
+    else if(!urlTxt.includes(unimiURL))
         ical = invalidURL
     else
         document.getElementById('ical-button').disabled = false
@@ -12,6 +36,7 @@ function urlChangeEvent() {
     document.getElementById('url-ical').value = ical
 }
 
+// put the ics url in the user clipboard
 function copyUrlToClipboard() {
     var url = document.getElementById('url-ical').value
     navigator.clipboard.writeText(url).then(function() {
@@ -21,6 +46,7 @@ function copyUrlToClipboard() {
     });
 }
 
+// notify the user the result of the copy function
 function displayResult(success) {
     var text = ""
     if(success)
@@ -33,6 +59,7 @@ function displayResult(success) {
     toggleAlertText(true)
 }
 
+// fade in/fade out result message
 function toggleAlertText(on) {
     if(on) {
         document.getElementById('result').style['animation'] = 'fadeIn 0.7s';
@@ -53,6 +80,7 @@ function toggleAlertText(on) {
     }
 }
 
+// check if browser support clipboard APIs
 if(!navigator.clipboard) {
     document.getElementById('ical-button').disabled = true
     document.getElementById('result').innerText = copyNotSupported
